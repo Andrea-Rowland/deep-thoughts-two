1242bae87619c3726f787a5d20602a22ee8e2420
nicksandoval <sand8val@gmail.com>
2022-03-30 20:49:51 -0500

    fixed + solved

--- 04-Web-APIs/01-Activities/03-Timers-Intervals/Solved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/03-Timers-Intervals/Solved/assets/js/script.js
@@ -5,23 +5,28 @@ var message =
   'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
 var words = message.split(' ');
 
+// Timer that counts down from 5
 function countdown() {
   var timeLeft = 5;
 
-  // TODO: Add a comment describing the functionality of the setInterval() method:
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
   var timeInterval = setInterval(function () {
-    // TODO: Add comments describing the functionality of the `if` statement:
+    // As long as the `timeLeft` is greater than 1
     if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
       timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
       timeLeft--;
-    } // TODO: Add comments describing the functionality of the `else if` statement:
-    else if (timeLeft === 1) {
+    } else if (timeLeft === 1) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
       timerEl.textContent = timeLeft + ' second remaining';
       timeLeft--;
-    } // TODO: Add comments describing the functionality of the `else` statement:
-    else {
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
       timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
       clearInterval(timeInterval);
+      // Call the `displayMessage()` function
       displayMessage();
     }
   }, 1000);
